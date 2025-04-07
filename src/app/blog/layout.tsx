28d2f0ae9e8a382d4591b5948de4f518eef55c3f
Bem Benjamin <arcanesmile999@gmail.com>
2025-04-07 17:35:42 +0100

    i

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -2,7 +2,7 @@ import BlogNav from '@/components/BlogNav';
 
 export default function BlogLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <div style={{ backgroundColor: '#e0e4eb' }}>
       <BlogNav />
       <main>{children}</main>
     </div>
